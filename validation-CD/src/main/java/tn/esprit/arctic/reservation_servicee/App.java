d77f69290db5f1800137d05d506fba841704c774
nada-aniba <nadaaniba1@gmail.com>
2019-11-24 20:22:44 +0100

    first commit

---
+++ validation-CD/src/main/java/tn/esprit/arctic/reservation_servicee/App.java
@@ -0,0 +1,13 @@
+package tn.esprit.arctic.reservation_servicee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
